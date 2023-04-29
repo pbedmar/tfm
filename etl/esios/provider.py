38f0972145320c70ece8ea636946713cb475b29b
pbedmar <correo.pedrobedmar@gmail.com>
2023-04-29 12:06:55 +0200

    pre-load data in ESIOS provider. add get_all_series() method.

--- etl/esios/provider.py
+++ etl/esios/provider.py
@@ -10,16 +10,20 @@ class ESIOSDataProvider(DataProvider):
     def __init__(self):
         self.path = "datalake/clean/esios/"
         self.metadata = Metadata(self.path)
+        self.tickers = self.metadata.get_tickers()
+        self.ts_df = pd.read_csv(self.path+self.tickers[0]+".csv", index_col="DATE")
+        for ticker in self.tickers[1:]:
+            ts = pd.read_csv(self.path+ticker+".csv", index_col="DATE")
+            self.ts_df = pd.concat([self.ts_df, ts], axis=1)
 
     def get_tickers(self):
-        return self.metadata.get_tickers()
+        return self.tickers
 
     def get_metadata(self, ticker=None):
         return self.metadata.get_metadata(ticker)
 
     def get_series(self, ticker, freq=None, start_index=None, end_index=None, resample_by=None, group_mode=None):
-        ts = pd.read_csv(self.path+ticker+".csv", index_col="DATE").squeeze()
-        #ts.index = pd.to_datetime(ts.index)
+        ts = self.ts_df[ticker].squeeze()
         if freq is not None:
             ts.index = pd.PeriodIndex(ts.index, freq=freq)
         ts = ts.sort_index()
@@ -36,6 +40,25 @@ class ESIOSDataProvider(DataProvider):
                 ts = ts.mean()
 
         return ts
+
+    def get_all_series(self, freq=None, start_index=None, end_index=None, resample_by=None, group_mode=None):
+        df = self.ts_df
+        if freq is not None:
+            df.index = pd.PeriodIndex(df.index, freq=freq)
+        df = df.sort_index()
+
+        if start_index is not None:
+            df = df.loc[df.index >= start_index]
+        if end_index is not None:
+            df = df.loc[df.index <= end_index]
+        if resample_by is not None and group_mode is not None:
+            df = df.resample(resample_by)
+            if group_mode == "sum":
+                df = df.sum()
+            if group_mode == "mean":
+                df = df.mean()
+
+        return df
 
 
 if __name__ == '__main__':
