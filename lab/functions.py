3502091e1c1b07ddcf38db9c821ff37706f09060
pbedmar <correo.pedrobedmar@gmail.com>
2023-06-21 21:06:28 +0200

    [analysis] Hourly analysis finished, both forecast and SHAP values

--- lab/functions.py
+++ lab/functions.py
@@ -1,13 +1,16 @@
+import os
 import pandas as pd
 import numpy as np
+import shap
 
 from sklearn.ensemble import IsolationForest, RandomForestRegressor, GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
+from sktime.forecasting.model_selection import temporal_train_test_split
 from sklearn.model_selection import GridSearchCV, TimeSeriesSplit, ShuffleSplit
 
 from sktime.utils.plotting import plot_series
-from sktime.performance_metrics.forecasting import mean_absolute_percentage_error
+from sktime.performance_metrics.forecasting import MeanAbsoluteScaledError, mean_absolute_error
 from sktime.transformations.series.difference import Differencer
 from sktime.forecasting.model_evaluation import evaluate
 from sktime.forecasting.model_selection import SlidingWindowSplitter
@@ -32,15 +35,96 @@ def remove_outliers_isolation_forests(data, contamination=0.05):
 
 
 ##########################################################################
-#                     Predictor importance analysis                      #
+#                           ML table computation                         #
 ##########################################################################
 
+def compute_lag(y, lags):
+    name = y.columns[0]
 
+    for lag in lags:
+        kwargs = {name+"_shift"+str(lag): y[name].shift(lag)}
+        y = y.assign(**kwargs)
+        # y = pd.concat([y, y[name].shift(lag)], axis=1)
+
+    y = y.drop(name, axis=1)
+    return y
+
+
+def compute_lag_fh_df(y, lags, fh):
+    return pd.concat([y, compute_lag(y, lags).shift(fh-1)], axis=1)
+
+
+def add_date_features(y, X, lags, fh, date_features):
+    y = pd.DataFrame(y)
+
+    if "year" in date_features:
+        X['year'] = y.index.year
+    if "month" in date_features:
+        X['month'] = y.index.month
+    if "day" in date_features:
+        X['day'] = y.index.day
+    if "hour":
+        X['hour'] = y.index.hour
+    if "day_of_week" in date_features:
+        X['day_of_week'] = y.index.day_of_week
+    if "day_of_year" in date_features:
+        X['day_of_year'] = y.index.day_of_year
+    if "week_of_year" in date_features:
+        X['week_of_year'] = y.index.weekofyear
+
+    df = pd.concat([compute_lag_fh_df(y, lags, fh), X], axis=1)
+    df = df.copy()
+    return df.dropna()
 
 
 ##########################################################################
 #                             Price forecast                             #
 ##########################################################################
+
+def crossval_model_sktime(y, X, regressor_list, regressor_str_list, window_length, max_lag, step_size, fh, save_path, save_name):
+    cv = SlidingWindowSplitter(fh, window_length, step_size)
+    #cv = ExpandingWindowSplitter(fh, window_length, step_size)
+
+    cv_results_list = []
+    for regressor, regressor_str in zip(regressor_list, regressor_str_list):
+        print(f"Training setup: regressor {regressor_str}")
+
+        # refit strategy makes a new model on each iteration, not an update
+        forecaster = make_reduction(regressor, strategy="direct", window_length=max_lag, windows_identical=True)
+        # cv_results = evaluate(forecaster, y=y, X=X, cv=cv, strategy="refit", error_score="raise", return_data=True, scoring=MeanAbsoluteScaledError(), backend="dask", compute=True)
+        cv_results = evaluate(forecaster, y=y, X=X, cv=cv, strategy="refit", error_score="raise", return_data=True, scoring=MeanAbsoluteScaledError())
+        cv_results_list.append(cv_results)
+
+    cv_results_df = pd.DataFrame(columns=list(cv_results_list[0].columns))
+    for element, regressor_str in zip(cv_results_list, regressor_str_list):
+        element["model"] = regressor_str
+        cv_results_df = pd.concat([cv_results_df, element], axis=0, ignore_index=True)
+
+    os.makedirs(save_path, exist_ok=True)
+    cv_results_df.to_pickle(save_path + save_name)
+    return cv_results_df
+
+
+def crossval_summary_sktime(cv_results_df):
+    cv_results_df_summary = pd.DataFrame(columns=['model', 'mae', 'mase', 'fit_time'])
+    for model in cv_results_df["model"].unique():
+        cv_results_model = cv_results_df[cv_results_df["model"] == model]
+
+        mae = 0
+        for test, pred in zip(cv_results_model["y_test"], cv_results_model["y_pred"]):
+            mae += mean_absolute_error(test, pred)
+        row = {'model': model,
+               'mae': mae,
+               'mase': cv_results_model["test_MeanAbsoluteScaledError"].mean(),
+               'fit_time': cv_results_model["fit_time"].sum()
+               }
+        row_df = pd.DataFrame([row])
+        cv_results_df_summary = pd.concat([cv_results_df_summary, row_df], axis=0, ignore_index=True)
+
+    cv_results_df_summary = cv_results_df_summary.sort_values(by="mase")
+    cv_results_df_summary
+    return cv_results_df_summary
+
 
 def crossval_window_size(y, X, forecaster, train_window_size_list, forecasting_horizon):
     cv_results_list = []
@@ -66,15 +150,18 @@ def crossval_window_size_make_reduction(y, X, regressor, train_window_size_list,
 
     return cv_results_list
 
-def crossval_plot_series(y, cv_results_list):
-    for cv_results in cv_results_list:
-        windows_nb = len(cv_results["y_pred"].tolist())
+def crossval_plot_series(y, cv_results_df):
+    for model in cv_results_df["model"].unique():
+        cv_results_model = cv_results_df[cv_results_df["model"]==model]
+        windows_nb = len(cv_results_model["y_pred"].tolist())
 
         fig, ax = plot_series(
             y,
-            *cv_results["y_pred"].tolist(),
+            *cv_results_model["y_pred"].tolist(),
             markers=["o"] + ["" for x in range(windows_nb)]
         )
+
+        ax.plot
 
 
 def relation_between_response_and_predictor(y, X, diff_lag):
@@ -93,6 +180,34 @@ def relation_between_response_and_predictor(y, X, diff_lag):
     forecaster.fit(y=y_transform, X=x_transform)
 
     return forecaster.get_fitted_params()
+
+
+##########################################################################
+#                         Predictor's influence                          #
+##########################################################################
+
+def predictors_influence_study(esios_spot, X, best_model, lags, date_features, forecasting_horizon, version="first", seed=0):
+    if version=="first":
+        df_date_features = add_date_features(esios_spot, X, lags, 1, date_features)
+    elif version=="last":
+        df_date_features = add_date_features(esios_spot, X, lags, forecasting_horizon, date_features)
+    else:
+        raise NotImplementedError
+
+    if version=="first":
+        df_date_features = df_date_features.iloc[:-forecasting_horizon, :]
+
+    y_date_features = df_date_features.iloc[:, 0]
+    X_date_features = df_date_features.iloc[:, 1:]
+
+    y_date_features_train, y_date_features_test, X_date_features_train, X_date_features_test = temporal_train_test_split(
+    y_date_features, X_date_features, test_size=0.15)
+
+    best_model.fit(X_date_features_train, y_date_features_train)
+    explainer = shap.Explainer(best_model.predict, X_date_features_test, seed=seed)
+    shap_values = explainer(X_date_features_test)
+
+    return shap_values
 
 
 if __name__ == '__main__':
