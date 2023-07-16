6daa678e3a97f69f4368ec6f608497169e15c6bb
pbedmar <correo.pedrobedmar@gmail.com>
2023-07-16 19:53:08 +0200

    finish experiments

--- lab/functions.py
+++ lab/functions.py
@@ -3,16 +3,14 @@ import warnings
 from statistics import mean
 from time import time
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import numpy as np
 import shap
 
 
-from sklearn.ensemble import IsolationForest, RandomForestRegressor, GradientBoostingRegressor
-from sklearn.svm import SVR
-from sklearn.neighbors import KNeighborsRegressor
-from sktime.forecasting.model_selection import temporal_train_test_split
-from sklearn.model_selection import GridSearchCV, TimeSeriesSplit, ShuffleSplit
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
 
 from sktime.utils.plotting import plot_series
 from sktime.performance_metrics.forecasting import MeanAbsoluteScaledError, mean_absolute_scaled_error, mean_absolute_error
@@ -213,7 +211,7 @@ def relation_between_response_and_predictor(y, X, diff_lag):
 
 def crossval_model_skforecast(data, regressor_list, regressor_str_list, initial_window_length, nb_windows, step_size, lags, date_features, forecasting_horizon):
     cv_results_df_summary = pd.DataFrame(0, index=np.arange(len(regressor_str_list)),
-                                         columns=["model", "mean_mase", "mean_mae", "fit_time"])
+                                         columns=["model", "mean_mase", "mean_mae", "modelling_time"])
     i = 0
 
     for regressor_str, regressor in zip(regressor_str_list, regressor_list):
@@ -222,7 +220,7 @@ def crossval_model_skforecast(data, regressor_list, regressor_str_list, initial_
         mase_list = []
         mae_list = []
 
-        total_fit_time = 0
+        start = time()
         for w in range(nb_windows):
             data_window = data[:initial_window_length + w * step_size]
 
@@ -236,25 +234,27 @@ def crossval_model_skforecast(data, regressor_list, regressor_str_list, initial_
             y_window_test_iso = data_window_test.iloc[:, 0]
             X_window_test_iso = data_window_test.iloc[:, 1:]
 
+            scaler = StandardScaler()
+            scaler.fit(X_window_train_iso)
+            X_window_train_scaled = scaler.transform(X_window_train_iso)
+            X_window_test_scaled = scaler.transform(X_window_test_iso)
+            X_window_train_scaled = pd.DataFrame(X_window_train_scaled, columns=X_window_train_iso.columns, index=X_window_train_iso.index)
+            X_window_test_scaled = pd.DataFrame(X_window_test_scaled, columns=X_window_test_iso.columns, index=X_window_test_iso.index)
+
             forecaster = ForecasterAutoreg(
                 regressor=regressor,
                 lags=lags
             )
 
-            start = time()
-
             forecaster.fit(
                 y=y_window_train_iso,
-                exog=X_window_train_iso
+                exog=X_window_train_scaled
             )
 
-            end = time()
-            total_fit_time += end-start
-
-            if X_window_test_iso.isna().sum().sum() == 0:
+            if X_window_test_scaled.isna().sum().sum() == 0:
                 y_pred = forecaster.predict(
                     steps=forecasting_horizon,
-                    exog=X_window_test_iso
+                    exog=X_window_test_scaled
                 )
 
                 y_pred = pd.Series(data=y_pred, index=y_window_test_iso.index)
@@ -267,17 +267,18 @@ def crossval_model_skforecast(data, regressor_list, regressor_str_list, initial_
                 mase_list.append(mase)
                 mae_list.append(mae)
 
+        end = time()
 
         print("Model:", regressor_str)
         print("MASEs:", mase_list)
         print("MAEs:", mae_list)
-        print("Fit time:", total_fit_time)
+        print("Modelling time:", end-start)
         print()
 
         cv_results_df_summary.iloc[i, 0] = regressor_str
         cv_results_df_summary.iloc[i, 1] = mean(mase_list)
         cv_results_df_summary.iloc[i, 2] = mean(mae_list)
-        cv_results_df_summary.iloc[i, 3] = total_fit_time
+        cv_results_df_summary.iloc[i, 3] = end-start
         i += 1
 
         fig, ax = plot_series(
@@ -289,32 +290,88 @@ def crossval_model_skforecast(data, regressor_list, regressor_str_list, initial_
 
     return cv_results_df_summary
 
+
+def final_model_skforecast(data_train, data_test, best_model, best_model_str, lags, date_features,
+                           forecasting_horizon, cv_results_df_summary, plot_lim, plot_title):
+
+    data_train_iso, data_test_iso = remove_outliers_isolation_forests_train_test(data_train, data_test,
+                                                                                 columns=[column for column in
+                                                                                          data_train.columns if
+                                                                                          column not in date_features])
+
+    y_train_iso = data_train_iso.iloc[:, 0]
+    X_train_iso = data_train_iso.iloc[:, 1:]
+    y_test_iso = data_test_iso.iloc[:, 0]
+    X_test_iso = data_test_iso.iloc[:, 1:]
+
+    scaler = StandardScaler()
+    scaler.fit(X_train_iso)
+    X_train_scaled = scaler.transform(X_train_iso)
+    X_test_scaled = scaler.transform(X_test_iso)
+    X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train_iso.columns, index=X_train_iso.index)
+    X_test_scaled = pd.DataFrame(X_test_scaled, columns=X_test_iso.columns, index=X_test_iso.index)
+
+    forecaster = ForecasterAutoreg(
+        regressor=best_model,
+        lags=lags
+    )
+
+    forecaster.fit(
+        y=y_train_iso,
+        exog=X_train_scaled,
+    )
+
+    y_pred = forecaster.predict(
+        steps=forecasting_horizon,
+        exog=X_test_scaled
+    )
+
+    y_pred = pd.Series(data=y_pred, index=y_test_iso.index)
+
+    residuals = abs(y_pred - y_test_iso)
+    RMSFE = np.sqrt(sum([x ** 2 for x in residuals]) / len(residuals))
+    band_size = 1.96 * RMSFE
+
+    if type(y_test_iso.index) is pd.core.indexes.period.PeriodIndex:
+        y_test_iso.index = y_test_iso.index.to_timestamp()
+    if type(y_pred.index) is pd.core.indexes.period.PeriodIndex:
+        y_pred.index = y_pred.index.to_timestamp()
+
+    fig, ax = plt.subplots(figsize=(13, 4))
+    sns.lineplot(data=y_test_iso, palette="deepskyblue", marker="o", ax=ax)
+    sns.lineplot(data=y_pred, palette="orange", marker="o", ax=ax)
+    ax.fill_between(y_test_iso.index, (y_test_iso - band_size), (y_test_iso + band_size), color='orange', alpha=.1)
+    ax.set_title(plot_title)
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Daily SPOT market price')
+    ax.set_ylim(plot_lim)
+    ax.legend(loc="upper right", labels=["real value", "prediction"])
+    plt.show()
+
+
 ##########################################################################
 #                         Predictor's influence                          #
 ##########################################################################
 
-def predictors_influence_study(esios_spot, X, best_model, lags, date_features, forecasting_horizon, version="first", seed=0):
-    if version=="first":
-        df_date_features = add_date_features(esios_spot, X, lags, 1, date_features)
-    elif version=="last":
-        df_date_features = add_date_features(esios_spot, X, lags, forecasting_horizon, date_features)
-    else:
-        raise NotImplementedError
+def predictors_influence_study(data_train, data_test, best_model):
 
-    if version=="first":
-        df_date_features = df_date_features.iloc[:-forecasting_horizon, :]
+    y_train = data_train.iloc[:, 0]
+    X_train = data_train.iloc[:, 1:]
+    y_test = data_test.iloc[:, 0]
+    X_test = data_test.iloc[:, 1:]
 
-    y_date_features = df_date_features.iloc[:, 0]
-    X_date_features = df_date_features.iloc[:, 1:]
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train.columns, index=X_train.index)
+    X_test_scaled = pd.DataFrame(X_test_scaled, columns=X_test.columns, index=X_test.index)
 
-    y_date_features_train, y_date_features_test, X_date_features_train, X_date_features_test = temporal_train_test_split(
-    y_date_features, X_date_features, test_size=0.15)
-
-    best_model.fit(X_date_features_train, y_date_features_train)
-    y_pred = best_model.predict(X_date_features_test)
-    print("Prediction MASE:", mean_absolute_scaled_error(y_date_features_test, y_pred, y_train=y_date_features_train))
-    explainer = shap.Explainer(best_model.predict, X_date_features_test, seed=seed)
-    shap_values = explainer(X_date_features_test)
+    best_model.fit(X_train_scaled, y_train)
+    y_pred = best_model.predict(X_test_scaled)
+    print("Prediction MASE:", mean_absolute_scaled_error(y_test, y_pred, y_train=y_train))
+    explainer = shap.Explainer(best_model.predict, X_test_scaled, seed=SEED)
+    shap_values = explainer(X_test_scaled)
 
     return shap_values
 
